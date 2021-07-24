7990e6d0869addd1d1b798d1a59efff6d70be3b7
Andrey Radkevich <andrey@48pacific.com>
2021-07-24 16:48:47 +0300

    ssr-material-fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Document, { Html, Head, NextScript, Main } from 'next/document'
 
+import { ServerStyleSheets } from '@material-ui/core/styles'
+
 class MyDocument extends Document {
   render() {
     return (
@@ -12,13 +14,31 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
-          <div id="ssr" />
-
           <Main />
           <NextScript />
         </body>
       </Html>
     )
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets()
+  const originalRenderPage = ctx.renderPage
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />)
+    })
+
+  const initialProps = await Document.getInitialProps(ctx)
+
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement()
+    ]
   }
 }
 
